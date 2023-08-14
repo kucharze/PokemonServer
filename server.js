8ec018e34fad784e7a3554e4464ff635ff68aa74
Zachary Kuchar <zacharyk323@live.com>
2023-08-14 10:17:37 -0400

    Set up backend models and send them to front end

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require("express");
 
+const pokemon = require("./models/pokemon");
 const app = express();
 
 const PORT = 3000;
 
 app.get("/", (req, res) => {
   res.send("Welcome to the Pokemon app!!");
+});
+
+app.get("/pokemon", (req, res) => {
+  res.send(pokemon);
 });
 
 app.listen(PORT, () => {

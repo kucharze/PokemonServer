3275f462df7c99a42419c7561d2b3732b92fd5ef
Zachary Kuchar <zacharyk323@live.com>
2023-08-14 10:15:09 -0400

    Aded route, server is set up and running

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const app = express();
 
 const PORT = 3000;
 
+app.get("/", (req, res) => {
+  res.send("Welcome to the Pokemon app!!");
+});
+
 app.listen(PORT, () => {
   console.log("Server running on port 3000");
 });

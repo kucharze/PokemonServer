dfba0a90c78d638fc3c800a4b767c08193ee4086
Zachary Kuchar <zacharyk323@live.com>
2023-08-14 10:30:45 -0400

    Setup index

---
+++ views/Index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Index() {
+    const myStyle = {
+        color: '#ffffff',
+        backgroundColor: '#000000',
+    };
+  return (
+    <div style={myStyle}>
+      <h1>See all the pokemon</h1>
+    </div>
+  )
+}
+
+export default Index

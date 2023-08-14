8777d5aeae846d3e636e1f94ccca9e0497f0c82a
Zachary Kuchar <zacharyk323@live.com>
2023-08-14 10:45:10 -0400

    Capitalize first letter

--- views/Index.jsx
+++ views/Index.jsx
@@ -5,12 +5,19 @@ function Index(props) {
         color: '#ffffff',
         backgroundColor: '#000000',
     };
+
+    const cap = (name) =>{
+        //https://www.freecodecamp.org/news/javascript-capitalize-first-letter-of-word/
+        let letter = name.charAt(0).toUpperCase()
+        let rem = name.slice(1)
+        return letter+rem
+    }
   return (
     <div style={myStyle}>
       <h1>See all the pokemon</h1>
       {
         props.pokemon.map((item)=>{
-            return <h1>{item.name}</h1>
+            return <h1>{cap(item.name)}</h1>
         })
       }
     </div>

b7bd684271c28ee466f03d216fe7edb8b185f805
Zachary Kuchar <zacharyk323@live.com>
2023-08-18 11:54:13 -0400

    Added method override and mongoose

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const app = express();
 
 const PORT = 3000;
 
+require("dotenv").config();
+
 //Middleware
 app.set("view engine", "jsx");
 app.engine("jsx", require("express-react-views").createEngine());
@@ -16,6 +18,24 @@ app.use(express.urlencoded({ extended: false }));
 
 //Allows for use of res.json
 app.use(express.json());
+
+//include the method-override package place this where you instructor places it
+const methodOverride = require("method-override");
+//...
+//after app has been defined
+//use methodOverride.  We'll be adding a query parameter to our delete form named _method
+app.use(methodOverride("_method"));
+
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+mongoose.connection.once("open", () => {
+  console.log("connected to mongo");
+});
 
 const pokeList = [];
 
